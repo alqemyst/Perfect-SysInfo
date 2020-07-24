2b609e8da6a7f4d22115c809e13602ed06ecf91f
alqemyst <nedim@zecic.org>
2020-07-24 19:35:05 +0200

    fixing Package.swift

--- Package.swift
+++ Package.swift
@@ -22,23 +22,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "perfect-sysinfo",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "PerfectSysInfo",
-            targets: ["PerfectSysInfo"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "PerfectSysInfo",
-            dependencies: []),
-        .testTarget(
-            name: "PerfectSysInfoTests",
-            dependencies: ["PerfectSysInfo"]),
-    ]
+	name: "perfect-sysinfo",
+	platforms: [
+		.macOS(.v10_15)
+	],
+	products: [
+		.library(name: "PerfectSysInfo", targets: ["PerfectSysInfo"]),
+	],
+	dependencies: [ ],
+	targets: [
+		.target(name: "PerfectSysInfo", dependencies: []),
+		.testTarget(name: "PerfectSysInfoTests", dependencies: ["PerfectSysInfo"]),
+	]
 )
